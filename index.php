154bd64f72230dba2cc2df996dc308c630d85c6d
John Swanson <john@red.yellow.blue>
2016-10-08 16:08:16 -0400

    index multiupdate

--- index.php
+++ index.php
@@ -13,15 +13,63 @@
 </head>
 
 
-<a href="javascript:(function()%7B%20var%20fb%20=%20document.createElement('script');%20fb.type%20=%20'text/javascript';%20fb.src%20=%20'https://getfirebug.com/firebug-lite.js%23startOpened';%20document.getElementsByTagName('body')%5B0%5D.appendChild(fb);%20%7D)();">Open Firebug</a>
 <body>
+
 
 	<script src="./js/jquery.js" type="text/javascript"></script>
 <script src="./js/jquery.ba-hashchange.js" type="text/javascript"></script>
 	
 
 	<script src="./script_viewer.js" type="text/javascript"></script>
-<script src="./script_editor.js"type="text/javascript" ></script>
+<?
+
+include('./connect.php');
+if (isset($_COOKIE['ID_myapp'])){ 
+	$email = $_COOKIE['ID_myapp']; 
+	$pass = $_COOKIE['Key_myapp'];
+	$query ="SELECT * FROM admin WHERE email = '$email' ";
+	foreach($dbh->query( $query ) as $info){ 		$userID=$info['ID'];
+		$name=$info['shortname'];
+		if($pass!=$info['password']){
+				header("Location:./index.php");
+				exit;
+			}
+			
+			else {
+			$admin='true'; 
+			$loggedin='true'; 
+
+
+
+echo'<a id=""editnow" href="#" onclick="showButtons(); return false;">Edit Page</a><a href="login.php?logout=now">Logout</a>';?>
+<a href="javascript:(function()%7B%20var%20fb%20=%20document.createElement('script');%20fb.type%20=%20'text/javascript';%20fb.src%20=%20'https://getfirebug.com/firebug-lite.js%23startOpened';%20document.getElementsByTagName('body')%5B0%5D.appendChild(fb);%20%7D)();">Open Firebug</a>
+<?
+			}		
+		}	
+	}
+
+
+else{
+	$go = addslashes($_GET['go']);
+//$go='admin';
+	if($go == 'admin'){
+	echo'	<div ID="adminlogin">
+			<a class="adminlogin" href="login.php" onclick =""> Login<i class"fa fa-star"></i></a>
+			</div>';?>
+<a href="javascript:(function()%7B%20var%20fb%20=%20document.createElement('script');%20fb.type%20=%20'text/javascript';%20fb.src%20=%20'https://getfirebug.com/firebug-lite.js%23startOpened';%20document.getElementsByTagName('body')%5B0%5D.appendChild(fb);%20%7D)();">Open Firebug</a>
+<?
+
+		}
+}
+
+	if($admin=='true'){
+?>
+	<script src="./script_editor.js"type="text/javascript" ></script>
+<style>.admin{display:block;}</style>
+<?
+}
+?>
+
 	<div ID="page">
 	</div>
 
@@ -38,7 +86,8 @@ window.hello();
 			if( x=='1'){ x='2'; 
 				mydefaultpage = ''+ldat.ID+'';
 			window.showpages(''+mydefaultpage+'');
-				//window.showelements(''+ mydefaultpage +'');
+
+		
 			}	
 		});	
 	});
@@ -59,8 +108,11 @@ $(window).hashchange(function(){
 		$.each(json.data,function(i,ldat){
 			if( x=='1'){ x='2'; 
 				mydefaultpage = ''+ldat.ID+'';
-			
+				
+		
 				window.showelements(''+ mydefaultpage +'');
+
+
 			}	
 		});	
 	});
@@ -72,7 +124,8 @@ $(window).hashchange(function(){
 			$.each(json.data,function(i,dat){
 				
 				  window.showelements(''+dat.ID+'');
-				 
+		 //window.showButtons('+dat.ID+');
+				 window.showEdit('+dat.ID+');
   			});
   		});
   }
